ac0bdb1ec7f3ec03d33c2876ced3bae7b11cac48
wpob <01168909@pw.edu.pl>
2024-10-24 23:04:21 +0200

    environment setup for future learning

---
+++ env_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y python3.8 python3.8-venv python3.8-dev python3-pip
+
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+
+wget https://developer.download.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
+chmod +x cuda_8.0.61_375.26_linux-run
+sudo ./cuda_8.0.61_375.26_linux-run --override --silent --toolkit
+
+echo 'export PATH=/usr/local/cuda-8.0/bin:$PATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+source ~/.bashrc
+
+python3.8 -m venv pytorch-venv
+source pytorch-venv/bin/activate
+
+pip install torch==0.4.0 -f https://download.pytorch.org/whl/cu80/stable
+
+echo "Installation completed. PyTorch 0.4.0 with CUDA 8.0 is set up with Python 3.8."
